da7276a272b72b78b88a2bf5200d4cdfbb06c89f
combi2k2 <cumbee2k2@gmail.com>
2022-12-09 03:16:20 +0100

    Add feature to functions which run one iteration of train or eval

--- utils/train.py
+++ utils/train.py
@@ -1,31 +1,27 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
 import numpy as np
-import pandas as pd
-from torch.utils import data
-import itertools
-import re
-import random
-import time
-from torch.autograd import Variable
 
-def train_epoch(train_loader, model, optimizer, loss_function, device, coef = 0, regularizer = None):
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+def run_train(train_ds, model, optimizer, loss_func, args, coef = 0, regularizer = None):
+    train_loader = DataLoader(train_ds, batch_size = args.batch_size, shuffle = True, num_workers = 8)
     train_mse = []
     
     for batch_idx, (inputs, target) in enumerate(train_loader):
-        inputs = inputs.to(device)
-        target = target.to(device)
+        inputs = inputs.to(args.device)
+        target = target.to(args.device)
         
         loss = 0
         
         for tgt in target.transpose(0, 1):
             out = model(inputs)
-            inputs = torch.cat([inputs[:, 2:], out], 1)
             
-            if coef:    loss += loss_function(out, tgt) + coef * regularizer(out, tgt)
-            else:       loss += loss_function(out, tgt)
+            if (train_ds.stack_x):  inputs = torch.cat([inputs[:, out.shape[1]:], out], 1)
+            else:                   inputs = torch.cat([inputs[:, 1:], torch.unsqueeze(out, 1)], 1)
+            
+            if coef:    loss += loss_func(out, tgt) + coef * regularizer(out, tgt)
+            else:       loss += loss_func(out, tgt)
 
         train_mse.append(loss.item() / target.shape[1])
         
@@ -33,26 +29,28 @@ def train_epoch(train_loader, model, optimizer, loss_function, device, coef = 0,
         optimizer.step()
         
         if (batch_idx % 20 == 19):
-            print(f'    Batch {batch_idx}: MSE = {train_mse[-1]}')
+            print(f'    Batch {batch_idx + 1}: MSE = {train_mse[-1]}')
     
-    return round(np.sqrt(np.mean(train_mse)), 5)
+    return  round(np.sqrt(np.mean(train_mse)), 5)
 
-def eval_epoch(valid_loader, model, loss_function, device):
+def run_eval(valid_ds, model, loss_function, args):
+    valid_loader = DataLoader(valid_ds, batch_size = args.batch_size, shuffle = False, num_workers = 8)
     valid_mse = []
     preds = []
     trues = []
     
     with torch.no_grad():
         for inputs, target in valid_loader:
-            inputs = inputs.to(device)
-            target = target.to(device)
+            inputs = inputs.to(args.device)
+            target = target.to(args.device)
             
             loss = 0
             ims = []
             
             for tgt in target.transpose(0, 1):
                 out = model(inputs)
-                inputs = torch.cat([inputs[:, 2:], out], 1)
+                if (valid_ds.stack_x):  inputs = torch.cat([inputs[:, out.shape[1]:], out], 1)
+                else:                   inputs = torch.cat([inputs[:, 1:], torch.unsqueeze(out, 1)], 1)
                 
                 loss += loss_function(out, tgt)
                 ims.append(out.cpu().data.numpy())
@@ -68,3 +66,35 @@ def eval_epoch(valid_loader, model, loss_function, device):
         valid_mse = round(np.sqrt(np.mean(valid_mse)), 5)
 
     return valid_mse, preds, trues
+
+if __name__ == '__main__':
+    from models.TF_Net.TFNet import TF_Net
+    from dataset import rbc_data
+    
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    
+    time_range  = 6
+    output_length = 4
+    input_length = 16
+    dropout_rate = 0
+    kernel_size = 3
+    
+    model = TF_Net(input_channels = input_length*2, output_channels = 2, kernel_size = kernel_size, 
+            dropout_rate = dropout_rate, time_range = time_range).to(device)
+    model = torch.nn.DataParallel(model)
+
+    data_prep = [torch.load('data/sample_0.pt')]
+    sample_dt = rbc_data(data_prep, list(range(100)), input_length + time_range - 1, output_length, True)
+    
+    optim = torch.optim.Adam(model.parameters(), 1e-4, betas = (0.9, 0.999), weight_decay = 4e-4)
+    
+    class iter_args:
+        def __init__(self):
+            self.device = device
+            self.batch_size = 32
+    
+    train_mse = run_train(sample_dt, model, optim, torch.nn.MSELoss(), iter_args())
+    valid_mse, preds, trues = run_eval(sample_dt, model, torch.nn.MSELoss(), iter_args())
+
+    print(f'Train MSE: {train_mse}')
+    print(f'Valid MSE: {valid_mse}')
