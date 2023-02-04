1ae56eaf2c28a686958c8f88253da9d6381ab33b
combi2k2 <cumbee2k2@gmail.com>
2023-02-04 14:41:26 +0100

    reduce size of MG_model to fit environment add residual connection between the generating layers

--- models/MG_model.py
+++ models/MG_model.py
@@ -23,28 +23,29 @@ class MG(nn.Module):
         self.mem_layers = nn.ModuleList()
         self.gen_layers = nn.ModuleList()
         
-        self.mem_layers.append(MGMemLayer(exponent, exponent + 1, exponent - 1, exponent + 1, C, C << 2, 1))
-        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C << 2, C << 2, 2))
-        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C << 2, C << 3, 3))
-        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C << 3, C << 4, 4))
-        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C << 4, C << 4, 5))
+        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C, C << 1, 1))
+        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C << 1, C << 2, 2))
+        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C << 2, C << 2, 3))
+        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C << 2, C << 3, 4))
+        self.mem_layers.append(MGMemLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, C << 3, C << 3, 5))
         
-        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_past_frames * C << 4, num_future_frames * C << 3, 6))
-        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_future_frames * C << 3, num_future_frames * C << 3, 7))
-        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_future_frames * C << 3, num_future_frames * C << 2, 8))
-        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_future_frames * C << 2, num_future_frames * C << 2, 9))
-        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_future_frames * C << 2, num_future_frames * C, 10))
+        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_past_frames * C << 3, num_future_frames * C << 3, 6))
+        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_future_frames * C << 3, num_future_frames * C << 2, 7))
+        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_future_frames * C << 2, num_future_frames * C << 2, 8))
+        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_future_frames * C << 2, num_future_frames * C << 1, 9))
+        self.gen_layers.append(MGConvLayer(exponent - 1, exponent + 1, exponent - 1, exponent + 1, num_future_frames * C << 1, num_future_frames * C, 10))
         
         self.out_layer = MGConvLayer(exponent - 1, exponent + 1, exponent, exponent, num_future_frames * C, num_future_frames * C, 100)
 
     def forward(self, inputs):
         N, T, C, H, W = inputs.size()
         inputs = inputs.view(N * T, C, H, W)
-        exponent = math.ceil(np.log(max(H, W)) / np.log(2))
+        exponent = math.ceil(np.log(max(H, W)) / np.log(2)) - 1
         
         inputs2scales = [
             F.interpolate(inputs, size = (1 << exponent, 1 << exponent), mode = 'nearest'),
-            F.interpolate(inputs, size = (2 << exponent, 2 << exponent), mode = 'nearest')
+            F.interpolate(inputs, size = (2 << exponent, 2 << exponent), mode = 'nearest'),
+            F.interpolate(inputs, size = (4 << exponent, 4 << exponent), mode = 'nearest')
         ]
         
         output_grids_list = []
@@ -68,9 +69,15 @@ class MG(nn.Module):
             prev_grids[i] = prev_grids[i].view(N, T * chan, height, width)
             
         # generator layers
-        for layer in self.gen_layers:
+        for lay_ind, layer in enumerate(self.gen_layers):
             output_dims, output_grids = layer(prev_grids)
-            prev_grids = output_grids
+            output_grids_list.append(output_grids)
+            
+            if lay_ind % 2 == 0 and lay_ind > 0:
+                for scale in range(len(output_grids)):
+                    output_grids_list[-1][scale] = self.residual_conn(output_grids_list[-3][scale], output_grids_list[-1][scale])
+            
+            prev_grids = output_grids_list[-1]
         
         out = self.out_layer(prev_grids)[1][0]
         out = out.view(N, self.num_future_frames, C, H, W)
@@ -87,11 +94,10 @@ class MG(nn.Module):
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(device)
     
     model = MG(frame_shape = (2, 64, 64)).to(device)
     
-    inputs = torch.randn(8, 16, 2, 64, 64).to(device)
+    inputs = torch.randn(16, 16, 2, 64, 64).to(device)
     output = model(inputs)
     
     print(output.shape)
