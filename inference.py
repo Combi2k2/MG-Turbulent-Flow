cc07f59772a4b5be806fdd8f42bec85275a3d4b7
combi2k2 <cumbee2k2@gmail.com>
2023-05-20 13:39:39 +0200

    Script to generate prediction from model checkpoints and test data

--- inference.py
+++ inference.py
@@ -1,3 +1,8 @@
+'''
+    Modify the MODEL_NAME and EXPERIMENT_NAME
+    Modify the CHECKPOINT_PATH to match the given experiment
+    Modify the TEST_DATA_PATH
+'''
 import torch
 
 from models import *
@@ -5,14 +10,8 @@ from models import *
 # set up device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def test(checkpoint_path, model, entries):
-    loaded_checkpoint = torch.load(checkpoint_path, map_location = device)
-    
-    model = model.to(device)
-    model.load_state_dict(loaded_checkpoint['best_model'])
-    model.eval()
-    
-    inputs = entries.unsqueeze(0).to(device)
+def predict(model, inputs):
+    inputs = inputs.unsqueeze(0).to(device)
     preds = inputs[0]
 
     # start inference loops
@@ -25,33 +24,41 @@ def test(checkpoint_path, model, entries):
     
     return preds
 
-if __name__ == '__main__':
-    checkpoint_path = 'checkpoints/FNO2d_rbc_data_16_4_checpoint.pth'
-    model = FNO2d((2, 64, 64), 16, 4, width = 128)
+MODEL_NAME = 'MGxTransformer'
+EXPERIMENT_NAME = 'rbc_data_16_4'
+TEST_DATA_PATH = 'data/sample_5.pt'
+CHECKPOINT_PATH = f'{MODEL_NAME}_{EXPERIMENT_NAME}_checkpoint.pth'
+
+loaded_checkpoint = torch.load(CHECKPOINT_PATH, map_location = device)
+
+model = MGxTransformer((2, 64, 64), 16, 4)
+model = model.to(device)
+model.load_state_dict(loaded_checkpoint['best_model'])
+model.eval()
     
-    # set up the entries of the turbu~lent flow
-    data = torch.load('data/sample_5.pt')
+# set up the entries of the turbu~lent flow
+data = torch.load(TEST_DATA_PATH)
+preds = predict(model, data[:16])
+
+torch.save(preds, f'demo/{MODEL_NAME}_{EXPERIMENT_NAME}.pt')
     
-    preds = test(checkpoint_path, model, data[:16])
-    preds = preds.data.cpu().numpy()
+# data = data.numpy()
+
+# from matplotlib import pyplot as plt
+# from matplotlib.animation import FuncAnimation
+
+# fig, axs = plt.subplots(2, 2)
+
+# axs[0, 0].set_title('Ground truth x')
+# axs[1, 0].set_title('Ground truth y')
+# axs[0, 1].set_title('Prediction x')
+# axs[1, 1].set_title('Prediction y')
+
+# def animate(i):
+#     axs[0, 0].imshow(data[i][0])
+#     axs[1, 0].imshow(data[i][1])
+#     axs[0, 1].imshow(preds[i][0])
+#     axs[1, 1].imshow(preds[i][1])
     
-    data = data.numpy()
-    
-    from matplotlib import pyplot as plt
-    from matplotlib.animation import FuncAnimation
-    
-    fig, axs = plt.subplots(2, 2)
-    
-    axs[0, 0].set_title('Ground truth x')
-    axs[1, 0].set_title('Ground truth y')
-    axs[0, 1].set_title('Prediction x')
-    axs[1, 1].set_title('Prediction y')
-    
-    def animate(i):
-        axs[0, 0].imshow(data[i][0])
-        axs[1, 0].imshow(data[i][1])
-        axs[0, 1].imshow(preds[i][0])
-        axs[1, 1].imshow(preds[i][1])
-        
-    ani = FuncAnimation(fig, animate, frames = 100, interval = 50, repeat = False)
-    plt.show()
+# ani = FuncAnimation(fig, animate, frames = 100, interval = 50, repeat = False)
+# plt.show()
